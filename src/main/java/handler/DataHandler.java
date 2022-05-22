02d837873a3972a7db4695b0f61f85395d5d63dd
Tomasz <lis109@wp.pl>
2022-05-23 01:33:43 +0200

    delete unused code and imports fix return values

--- src/main/java/handler/DataHandler.java
+++ src/main/java/handler/DataHandler.java
@@ -12,46 +12,29 @@ import java.net.http.HttpResponse;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionException;
 import java.util.stream.Collectors;
 
 public class DataHandler implements DataProvider {
 
-    public ArrayList<SpreadItem> getSpreadData() {
-        ArrayList<SpreadItem> spreadItems = new ArrayList<>();
-        MarketPairsProvider marketPairsProvider = new MarketPairHandler(); // here can I handle exception
+    public List<SpreadItem> getSpreadData() {
+        MarketPairsProvider marketPairsProvider = new MarketPairHandler();
         List<URI> marketUrls = marketPairsProvider.getmarketPairsData();
-        // for (String marketPair : marketPairsProvider.getmarketPairsData()) {
         try {
-            // System.out.println("Values for pair: " + marketPair);
-            // HttpClient client = HttpClient.newHttpClient();
-            // HttpRequest request = HttpRequest.newBuilder()
-            //         .uri(URI.create("https://public.kanga.exchange/api/market/orderbook/" + marketPair))
-            //         .timeout(Duration.ofSeconds(20L))
-            //         .build();
-            // SpreadItem item = client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
-            //                 .thenApply(stringHttpResponse -> stringHttpResponse.body())
-            //                 .thenApply(JsonParser::spreadParser)
-            //                 .join();
-            // spreadItems.add(item);
-
             HttpClient client = HttpClient.newHttpClient();
             List<SpreadItem> items = marketUrls.stream()
                     .map(marketUrl -> client
                             .sendAsync(
-                                    HttpRequest.newBuilder(marketUrl).GET().build(),
+                                    HttpRequest.newBuilder(marketUrl).GET().timeout(Duration.ofSeconds(20L)).build(),
                                     HttpResponse.BodyHandlers.ofString())
                             .thenApply(stringHttpResponse -> stringHttpResponse.body())
                             .thenApply(JsonParser::spreadParser).join())
                     .collect(Collectors.toList());
-                    //spreadItems.add(item);
-
+            return items;
         } catch (CompletionException c) {
             System.out.println("Can not load page " + c.getMessage());
             return new ArrayList<>();
         }
-        return spreadItems;
     }
 
 
@@ -69,7 +52,7 @@ public class DataHandler implements DataProvider {
     public static void main(String[] args) {
         DataHandler dataHandler = new DataHandler();
 
-        ArrayList<SpreadItem> spreadList = dataHandler.getSpreadData();
+        List<SpreadItem> spreadList = dataHandler.getSpreadData();
 
 
     }
